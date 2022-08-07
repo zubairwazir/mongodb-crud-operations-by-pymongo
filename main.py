a924ee4b9e821fd6d49d75d4ffbb348ae239afdb
zubairwazir <zubairwazir777@gmail.com>
2022-08-07 14:41:03 +0500

    add called functions

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from model import UserModel, DeviceModel, WeatherDataModel
+from model import UserModel, DeviceModel, WeatherDataModel, DailyReportModel
 from datetime import datetime
 
 
+# Prompt for input Role from User
+role = input("Enter Role admin/default: ")
+
 # Shows how to initiate and search in the users collection based on a username
 user_coll = UserModel()
-# Prompt for input Role from User
-role = input("Enter Role: ")
 user_document = user_coll.find_by_username(role)
 if user_document:
     print(user_document)
@@ -19,12 +20,12 @@ else:
 
 # Shows how to initiate and search in the devices collection based on a device id
 device_coll = DeviceModel()
-device_document = device_coll.find_by_device_id('DT002')
+device_document = device_coll.find_by_device_id('DT002', role)
 if device_document:
     print(device_document)
 
 # Shows a successful attempt on how to insert a new device
-device_document = device_coll.insert('DT201', 'Temperature Sensor', 'Temperature', 'Acme')
+device_document = device_coll.insert('DT201', 'Temperature Sensor', 'Temperature', 'Acme', role)
 if device_document == -1:
     print(device_coll.latest_error)
 else:
@@ -33,12 +34,26 @@ else:
 
 # Shows how to initiate and search in the weather_data collection based on a device_id and timestamp
 wdata_coll = WeatherDataModel()
-wdata_document = wdata_coll.find_by_device_id_and_timestamp('DT002', datetime(2020, 12, 2, 13, 30, 0))
+wdata_document = wdata_coll.find_by_device_id_and_timestamp('DT002', datetime(2020, 12, 2, 13, 30, 0), role)
 if wdata_document:
     print(wdata_document)
 
 # Shows a failed attempt on how to insert a new data point
-wdata_document = wdata_coll.insert('DT002', 12, datetime(2020, 12, 2, 13, 30, 0))
+wdata_document = wdata_coll.insert('DT002', 12, datetime(2020, 12, 2, 13, 30, 0), role)
+if wdata_document == -1:
+    print(wdata_coll.latest_error)
+else:
+    print(wdata_document)
+
+
+# Shows how to initiate and search in the daily report collection based on a device_id and day range
+reports_coll = DailyReportModel()
+reports_document = reports_coll.find_by_device_id('DT002', datetime(2020, 12, 2, 13, 30, 0))
+if reports_document:
+    print(reports_document)
+
+# Shows a failed attempt on how to insert a new data point
+wdata_document = wdata_coll.insert('DT002', 12, datetime(2020, 12, 2, 13, 30, 0), role)
 if wdata_document == -1:
     print(wdata_coll.latest_error)
 else:
