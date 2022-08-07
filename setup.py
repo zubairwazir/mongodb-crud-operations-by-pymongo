a83b7fcb7a1b00ad75ecb9efb20fe68420ae6f31
zubairwazir <zubairwazir777@gmail.com>
2022-08-07 14:41:40 +0500

    adding daily reports collection to weather_db database

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ DB_NAME = 'weather_db'
 USER_COLLECTION = 'users'
 DEVICE_COLLECTION = 'devices'
 WEATHER_DATA_COLLECTION = 'weather_data'
+DAILY_REPORTS_COLLECTION = 'daily_reports'
 
 # This will initiate connection to the mongodb
 db_handle = MongoClient(f'mongodb://{HOST}:{PORT}')
@@ -43,9 +44,8 @@ with open(RELATIVE_CONFIG_PATH+USER_COLLECTION+'.csv', 'r') as user_fh:
         user_collection.insert_one(user_data)
 
 
-# device data import
-# Device document contains device_id (String), desc (String), type (String - temperature/humidity) and manufacturer (String) fields
-# Reads devices.csv one line at a time, splits them into the data fields and inserts
+# device data import Device document contains device_id (String), desc (String), type (String - temperature/humidity)
+# and manufacturer (String) fields Reads devices.csv one line at a time, splits them into the data fields and inserts
 with open(RELATIVE_CONFIG_PATH+DEVICE_COLLECTION+'.csv', 'r') as device_fh:
     for device_row in device_fh:
         device_row = device_row.rstrip()
@@ -63,7 +63,7 @@ with open(RELATIVE_CONFIG_PATH+DEVICE_COLLECTION+'.csv', 'r') as device_fh:
 # weather data generation
 # Weather data document contains device_id (String), value (Integer), and timestamp (Date) fields
 # Reads devices.csv one line at a time to get device id and type. It then loops for five days (2020-12-01 to 2020-12-05
-# For each device and day, it creates random values for each hour (at the 30 minute mark) and stores the data
+# For each device and day, it creates random values for each hour (at the 30-minute mark) and stores the data
 with open(RELATIVE_CONFIG_PATH+DEVICE_COLLECTION+'.csv', 'r') as device_fh:
     for device_row in device_fh:
         device_row = device_row.rstrip()
@@ -73,11 +73,12 @@ with open(RELATIVE_CONFIG_PATH+DEVICE_COLLECTION+'.csv', 'r') as device_fh:
         for day in range(1,6):
             for hour in range(0,24):
                 timestamp = datetime(2020, 12, day, hour, 30, 0)
-                # Generates random data value in appropriate range as per the type of sensor (normal bell-curve distribution)
-                if (type.lower() == 'temperature'):
-                    value = int(random.normalvariate(24,2.2))
-                elif (type.lower() == 'humidity'):
-                    value = int(random.normalvariate(45,3))
+                # Generates random data value in appropriate range as per the type of sensor (normal bell-curve
+                # distribution)
+                if type.lower() == 'temperature':
+                    value = int(random.normalvariate(24, 2.2))
+                elif type.lower() == 'humidity':
+                    value = int(random.normalvariate(45, 3))
                 weather_data = {'device_id': device_id, 'value': value, 'timestamp': timestamp}
                 
                 # This creates and return a pointer to the weather_data collection
@@ -85,3 +86,20 @@ with open(RELATIVE_CONFIG_PATH+DEVICE_COLLECTION+'.csv', 'r') as device_fh:
                 
                 # This inserts the data item as a document in the weather_data collection
                 weather_data_collection.insert_one(weather_data)
+
+
+with open(RELATIVE_CONFIG_PATH + DAILY_REPORTS_COLLECTION + '.csv', 'r') as reports_fh:
+    for reports_row in reports_fh:
+        reports_row = reports_row.rstrip()
+        if reports_row:
+            (device_id, avg_value, min_value, max_value, date) = reports_row.split(',')
+        reports_data = {'device_id': device_id, 'avg_value': avg_value, 'min_value': min_value, 'max_value': max_value, 'date': date}
+
+        # This creates and return a pointer to the users collection
+        reports_collection = weather_dbh[DAILY_REPORTS_COLLECTION]
+
+        # This inserts the data item as a document in the user collection
+        reports_collection.insert_one(reports_data)
+
+
+
